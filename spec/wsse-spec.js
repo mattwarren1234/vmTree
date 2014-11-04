24bd4fb3500dc2a14a8dab8a4c4c08b73c45c0b7
Matt Warren <mattwarrenmail@gmail.com>
2014-11-03 19:40:21 -0800

    added new set of tests for wsse library

---
+++ spec/wsse-spec.js
@@ -0,0 +1,51 @@
+var wsse = require('wsse')();
+// http://www.xml.com/pub/a/2003/12/17/dive.html
+
+describe('Wsse', function() {
+  describe('getNonce', function() {
+
+    it('works', function(done) {
+      expect(wsse).toBeDefined();
+      // expect(wsse.getNonce).toBeDefined();
+      // var nonce = wsse.getNonce();
+      // expect(nonce.created).toBeDefined();
+      // expect(nonce).toBeDefined();
+      done();
+    });
+
+  });
+
+  describe('getPasswordDigest', function() {
+
+    it('works', function(done) {
+      var expected = 'quR/EWLAV4xLf9Zqyw4pDmfV9OY=';
+      var password = 'taadtaadpstcsm';
+      var nonce = 'd36e316282959a9ed4c89851497a717f';
+      var created = '2003-12-15T14:43:07Z';
+      var actual = wsse.getPasswordDigest(nonce, created, password);
+      expect(actual).toEqual(expected);
+      done();
+    });
+
+  });
+
+  describe('getUsernameToken', function() {
+
+    it('works', function() {
+      var original = wsse.getNonce;
+      wsse.getNonce = function() {
+        return { created: '2003-12-15T14:43:07Z', nonce: 'd36e316282959a9ed4c89851497a717f' };
+      };
+
+      var expected = 'Username="bob", PasswordDigest="quR/EWLAV4xLf9Zqyw4pDmfV9OY=", Nonce="d36e316282959a9ed4c89851497a717f", Created="2003-12-15T14:43:07Z"';
+      var username = 'bob';
+      var password = 'taadtaadpstcsm';
+      var actual = wsse.getUsernameToken(username, password);
+      expect(actual).toEqual(expected);
+
+      wsse.getNonce = original;
+    });
+
+  });
+
+});
