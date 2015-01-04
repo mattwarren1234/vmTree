34e9c5d21aa560d346ea4d3ae251036be3b258b6
Matt Warren <mattwarrenmail@gmail.com>
2015-01-03 20:52:49 -0800

    all vm calls now work

--- services/vmApi.js
+++ services/vmApi.js
@@ -1,3 +1,4 @@
+'use strict';
 var request = require('request');
 var wsse = require('wsse')();
 var http = require('http');
@@ -5,7 +6,6 @@ var qs = require('querystring');
 var sha256 = require('sha256');
 var btoa = require('btoa');
 var privateKey = require('./privateKey');
-'use strict';
 
 var buildQuery = function(action, searchOptions){
   var baseUrl = "http://www.volunteermatch.org/api/call";
@@ -18,9 +18,27 @@ var buildQuery = function(action, searchOptions){
 };
 
 var searchOrganizations = function(criteria){
+  ///criteria:
   var action = "searchOrganizations";
   var searchOptions = criteria;
   var options = buildQuery(action, searchOptions);
+  return new Promise(function (resolve, reject) { 
+    request(options, function (error, response, body) {
+      if (!error && response.statusCode == 200) {
+        resolve(body); 
+      } else {
+        reject(error);
+      }
+    });
+  });
+};
+
+var searchOpportunities = function(criteria){
+  var action = "searchOpportunities";
+  var searchOptions = criteria;
+  var options = buildQuery(action, searchOptions);
+  // console.log('url is ' + options);
+  // console.log(options);
   return new Promise(function (resolve, reject) { 
     request(options, function (error, response, body) {
       if (!error && response.statusCode == 200) {
@@ -68,5 +86,6 @@ var helloWorld = function(){
 
 exports.getCategories = getCategories;
 exports.buildQuery = buildQuery;
+exports.searchOpportunities = searchOpportunities;
 exports.searchOrganizations = searchOrganizations;
 exports.helloWorld = helloWorld;
--- spec/vmApi-spec.js
+++ spec/vmApi-spec.js
@@ -12,43 +12,6 @@ var isJSON = function(input){
         return false;
     }
 };
-xdescribe('query builder', function(){
-    it('should return string', function(){
-        expect(vmApi.buildQuery).toBeDefined();
-        var action = "getMetaData";
-        var options = {version : 1};
-        expect(typeof vmApi.buildQuery(action, options)).toBe('string');
-        console.log(vmApi.buildQuery(action, options));
-    });
-});
-
-xdescribe('getCategories', function(){
-    it('should ', function(done){
-        expect(vmApi.getCategories).toBeDefined();
-        vmApi.getCategories().
-            then(function(response){
-                expect(response).toBeDefined();
-                done();
-            });
-    });
-});
-
-describe('searchOrganizations', function(){
-    it('should ', function(done){
-        var searchCriteria = {
-            "location" : "94108",
-            "nbOfResults": 10,
-            "fieldsToDisplay": [ "name", "location", "url" ],
-            "names" : [ "red cross" ]
-        };
-        expect(vmApi.searchOrganizations).toBeDefined();
-        vmApi.searchOrganizations(searchCriteria).
-            then(function(response){
-                expect(response).toBeDefined();
-                done();
-            });
-    });
-});
 
 describe('helloWorld', function(){
     it ('function defined', function(){
@@ -64,3 +27,134 @@ describe('helloWorld', function(){
             });
     });
 });
+
+xdescribe('query builder', function(){
+    it('should return string', function(){
+        expect(vmApi.buildQuery).toBeDefined();
+        var action = "getMetaData";
+        var options = {version : 1};
+        expect(typeof vmApi.buildQuery(action, options)).toBe('string');
+        console.log(vmApi.buildQuery(action, options));
+    });
+});
+
+describe('getCategories', function(){
+    it('should ', function(done){
+        expect(vmApi.getCategories).toBeDefined();
+        vmApi.getCategories().
+            then(function(response){
+                expect(response).toBeDefined();
+                done();
+            });
+    });
+});
+
+describe('searchOrganizations', function(){
+    var redCrossSearch  = {
+            "location" : "94108",
+            "nbOfResults": 10,
+            "fieldsToDisplay": [ "name", "location", "url" ],
+            "names" : [ "red cross" ]
+        };
+    var manySearch = {
+        "location" : "92117",
+        "fieldsToDisplay": [ "name", "location", "url" ],
+    };
+    it('should ', function(done){
+        var searchCriteria = manySearch;
+        expect(vmApi.searchOrganizations).toBeDefined();
+        vmApi.searchOrganizations(searchCriteria).
+            then(function(response){
+                expect(response).toBeDefined();
+                expect(isJSON(response)).toBe(true);
+                expect(JSON.parse(response).resultsSize).toBeGreaterThan(-1);
+                done();
+            });
+    });
+});
+
+describe('searchOpportunities', function(){
+    //title, type, location, availability, parentOrg, beneficiary, description, plaintextDescription, skillsNeeded, plaintextSkillsNeeded, volunteersNeeded, spacesAvailable, minimumAge, numReferred, requiresAddress, categoryIds, referralFields, imageUrl, created, updated, allowGroupInvitations, allowGroupReservation, hasWaitList, status, tags, virtual, vmUrl, requirements
+    var testSearch = {
+        "location" : "92117",
+        "radius":"2"
+    };
+        // "numberOfResults": "2",
+        // "fieldsToDisplay": ["title"],
+        // "categoryIds" : ["11","17"]
+    it('should return list of opps', function(done){
+        // var searchCriteria = testSearch;
+        console.log('search criteria ');
+        var searchCriteria = clone(testSearch);
+        vmApi.searchOpportunities(searchCriteria).
+            then(function(response){
+                expect(response).toBeDefined();
+                expect(isJSON(response)).toBe(true);
+                var result = JSON.parse(response);
+                expect(result.resultsSize).toBeGreaterThan(-1);
+                console.log("search opps: total results " + result.resultsSize);
+                done();
+            });
+    });
+
+    var clone = function(obj){
+        return JSON.parse(JSON.stringify(obj));
+    };
+
+    it('changing virtual param should change number of results', function(done){
+        // var searchCriteria = testSearch;
+        var virtualParams = clone(testSearch);
+        virtualParams.virtual = true;
+        var nonVirtualParams = clone(testSearch);
+        nonVirtualParams.virtual = false;
+
+        var count1, count2;
+
+        vmApi.searchOpportunities(virtualParams)
+            .then(function(response){
+                expect(isJSON(response)).toBe(true);
+                var result = JSON.parse(response);
+                expect(result.resultsSize).toBeGreaterThan(-1);
+                count1 = result.resultsSize;
+            })
+            .then(function(){ return vmApi.searchOpportunities(nonVirtualParams); })
+            .then(function(response){
+                var result = JSON.parse(response);
+                count2 = result.resultsSize;
+                expect(count1).not.toEqual(count2);
+                done();
+            });
+    });
+    it('opportunities should be defined', function(done){
+        // var searchCriteria = testSearch;
+        var search = clone(testSearch);
+        // search.fieldsToDisplay = ["title", "location","vmUrl"];
+        vmApi.searchOpportunities(search)
+            .then(function(response){
+                expect(isJSON(response)).toBe(true);
+                var result = JSON.parse(response);
+                var opp = result.opportunities[0];
+                expect(opp.vmUrl).toBeDefined();
+                expect(opp.title).toBeDefined();
+                expect(opp.location).toBeDefined();
+                expect(result.opportunities).toBeDefined();  //array with props availability, categoryIds
+                done();
+            });
+    });
+    it('number of results should control results', function(done){
+        var search = clone(testSearch);
+        var numResults = 10;
+        search.numberOfResults = numResults;
+        vmApi.searchOpportunities(search)
+            .then(function(response){
+                var results = JSON.parse(response).opportunities;
+                console.log('number of results : ' + results.length + ' expected : ' + numResults);
+                expect(results.length).toEqual(numResults);
+                done();
+            });
+
+    });
+
+    // numberOfResults
+});
+
